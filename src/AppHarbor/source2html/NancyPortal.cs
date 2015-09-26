ac458d751f252855130a194655addb68017a6786
Ralf Westphal <info@ralfw.de>
2015-09-26 17:27:36 +0200

    added appharbor sln

---
+++ src/AppHarbor/source2html/NancyPortal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace source2html
+{
+    public class NancyPortal : Nancy.NancyModule
+    {
+        public NancyPortal()
+        {
+			Get ["/"] = p => "hello " + DateTime.Now.ToString ();
+        }
+    }
+}
