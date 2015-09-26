592e78460aef2d8bdf6820ecbfdc3cf8415372be
Ralf Westphal <info@ralfw.de>
2015-09-26 16:24:30 +0200

    initial

---
+++ src/source2html/nancyportal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web;
+using System.Net;
+
+using Nancy.Responses;
+
+namespace source2html
+{
+	// ...?lexer=csharp&linenos=1&ashtml=1&codeurl=https://gist.githubusercontent.com/ralfw/42b0071e49ce12c9ecd4/raw/c07b073ceb9fbdac8f9904766e4d171b2a7ed8b4/01%2520program.cs
+	// http://127.0.0.1:8080/?lexer=csharp&linenos=1&ashtml=1&codeurl=https://gist.githubusercontent.com/ralfw/42b0071e49ce12c9ecd4/raw/c07b073ceb9fbdac8f9904766e4d171b2a7ed8b4/01%2520program.cs
+
+	// http://hilite.me/api
+	// lexers: http://pygments.org/docs/lexers/, csharp, fsharp, js, java, cpp
+	// styles: http://pygments.org/docs/styles/#builtin-styles, colorful
+
+	public class Nancyportal : Nancy.NancyModule
+	{
+		public Nancyportal ()
+		{
+			// usage: ...?codeurl=...&lexer=...&linenos=1&ashtml=1
+			Get ["/"] = x => {
+				var codeurl = Request.Query["codeurl"];
+
+				var code = "";
+				using(var wc = new WebClient()) {
+					code = wc.DownloadString(codeurl);
+					code = HttpUtility.UrlEncode(code);
+				}
+
+				var lexer = Request.Query["lexer"];
+				var linenos = Request.Query["linenos"];
+				var ashtml = Request.Query["ashtml"];
+
+				var hiliteurl = string.Format("http://hilite.me/api?code={0}&lexer={1}&linenos={2}", code, lexer, linenos);
+				var html = "";
+				using(var wc = new WebClient()) {
+					html = wc.DownloadString(hiliteurl);
+				}
+
+				var htmlbytes = System.Text.Encoding.UTF8.GetBytes(html);
+				return new Nancy.Response {
+					ContentType = ashtml.HasValue ? "text/html; charset=utf-8" : "text/plain; charset=utf-8",
+					Contents = s => s.Write(htmlbytes, 0, htmlbytes.Length)
+				};
+			};
+		}
+	}
+}
